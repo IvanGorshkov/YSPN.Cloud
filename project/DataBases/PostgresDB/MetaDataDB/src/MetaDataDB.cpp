e3f23e1f15b2443516063e85fe59589b7cf68460
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2020-11-12 14:23:53 +0300

    Сделал тесты

--- project/DataBases/PostgresDB/MetaDataDB/src/MetaDataDB.cpp
+++ project/DataBases/PostgresDB/MetaDataDB/src/MetaDataDB.cpp
@@ -13,32 +13,33 @@ MetaDataDB::~MetaDataDB() {
 }
 
 void MetaDataDB::InsertFile() {
-
+  if (!connect()) { return; }
 }
 
 void MetaDataDB::InsertChunk() {
-
+  if (!connect()) { return; }
 }
 
 void MetaDataDB::InsertFileChunk() {
-
+  if (!connect()) { return; }
 }
 
 void MetaDataDB::SelectFile() {
-
+  if (!connect()) { return; }
 }
 
 void MetaDataDB::SelectChunk() {
-
+  if (!connect()) { return; }
 }
 
 void MetaDataDB::SelectFileChunk() {
-
+  if (!connect()) { return; }
 }
 
 void MetaDataDB::SelectFilesByUser() {
-
+  if (!connect()) { return; }
 }
+
 void MetaDataDB::createDB() {
 
 }
