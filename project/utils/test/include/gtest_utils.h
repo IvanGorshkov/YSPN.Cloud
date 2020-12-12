41330c4f386f8f99c4941b3ddef807d056b81d2b
Alekhin Sergey <alekhin1991@gmail.com>
2020-12-13 00:02:37 +0300

    Add utils tests

--- project/utils/test/include/gtest_utils.h
+++ project/utils/test/include/gtest_utils.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <gtest/gtest.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 #include "SerializerAnswer.h"
 #include "SerializerChunk.h"
 #include "SerializerUserChunk.h"
