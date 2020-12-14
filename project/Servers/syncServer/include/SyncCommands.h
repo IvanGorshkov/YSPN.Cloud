59a2c62cd5b8624d72bb9f4cc4ce5d3bc614af03
Alekhin Sergey <alekhin1991@gmail.com>
2020-12-15 01:38:43 +0300

    add sync server, no postgreSQL to write changes

---
+++ project/Servers/syncServer/include/SyncCommands.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <boost/property_tree/ptree.hpp>
+#include "Command.h"
+#include "SerializerAnswer.h"
+#include "SerializerUserDate.h"
+#include "SerializerFileMeta.h"
+
+namespace pt = boost::property_tree;
+
+class DataUpdateCommand : public Command {
+ public:
+  explicit DataUpdateCommand(std::shared_ptr<pt::ptree> &request);
+  std::shared_ptr<pt::ptree> Do() override;
+
+ private:
+  std::shared_ptr<pt::ptree> _request;
+  SerializerUserDate _userDate;
+};
+
+class UploadFileCommand : public Command {
+ public:
+  explicit UploadFileCommand(std::shared_ptr<pt::ptree> &request);
+  std::shared_ptr<pt::ptree> Do() override;
+
+ private:
+  std::shared_ptr<pt::ptree> _request;
+  SerializerFileMeta _fileMeta;
+};
--- project/Servers/syncServer/src/SyncServer.cpp
+++ project/Servers/syncServer/src/SyncServer.cpp
@@ -1,35 +1,51 @@
 #include "SyncServer.h"
+#include <boost/log/trivial.hpp>
 
-SyncServer::SyncServer(MetaDataDB *_db)
-    : _metaDataDB(_db) {
+SyncServer::SyncServer()
+    : _config(Config::GetInstance()),
+      _networkServer(std::make_shared<NetworkServer>(_config.GetNetworkConfig().port,
+                                                    _config.GetNetworkConfig().backlog)) {
+  BOOST_LOG_TRIVIAL(debug) << "SyncServer: create storage server";
 }
 
-void SyncServer::runNetworkServer() {
+SyncServer::~SyncServer() {
+  BOOST_LOG_TRIVIAL(debug) << "StorageServer: delete storage server";
+  stopWorkers();
 }
 
-void SyncServer::listeningConnection() {
+void SyncServer::Run() {
+  BOOST_LOG_TRIVIAL(info) << "SyncServer: Run";
+  startWorkers();
+  try {
+    _networkServer->StartServer();
+  } catch (ErrorRunServerException &er) {
+    stopWorkers();
+  }
 }
 
-void SyncServer::onConnect(UserSession *session) {
+void SyncServer::startWorkers() {
+  BOOST_LOG_TRIVIAL(debug) << "SyncServer: startWorkers";
+
+  auto workersCount = _config.GetServerConfig().workersCount;
+  _workerThreads.reserve(workersCount);
+
+  for (int i = 0; i < workersCount; ++i) {
+    _workerThreads.emplace_back(&Worker::Run,
+                                std::move(Worker(std::make_shared<SyncManager>(), _networkServer)));
+
+    BOOST_LOG_TRIVIAL(info) << "SyncServer: add new worker with id = " << _workerThreads[i].get_id();
+  }
 }
 
-void SyncServer::uploadFileMeta() {
+void SyncServer::stopWorkers() {
+  BOOST_LOG_TRIVIAL(debug) << "SyncServer: stopWorkers";
+  if (!_workerThreads.empty()) {
+    for (auto &thread : _workerThreads) {
+      thread.join();
+      BOOST_LOG_TRIVIAL(info) << "SyncServer: join worker with id = " << thread.get_id();
+    }
+
+    _workerThreads.clear();
+  }
 }
 
-void SyncServer::downloadFileMeta() {
-}
-
-void SyncServer::uploadChunkMeta() {
-}
-
-void SyncServer::downloadChunkMeta() {
-}
-
-void SyncServer::uploadFileChunksMeta() {
-}
-
-void SyncServer::downloadFileCHunksMeta() {
-}
-
-void SyncServer::findFiles() {
-}
