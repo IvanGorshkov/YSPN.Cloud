33016d242a5726a28a3e89dd789f168368dd960f
Max Torzhkov <makstorzhkov@yandex.ru>
2020-12-19 03:38:42 +0300

    chunker fix

--- project/Client/Chunker/tests/test_Chunker.cpp
+++ project/Client/Chunker/tests/test_Chunker.cpp
@@ -2,15 +2,15 @@
 #include <thread>
 
 TEST(TestChunker, RunTest) {
-    File file("/home/dantedoyl/watch/20new.txt");
+    File file("/home/dantedoyl/watch/asd.docx");
     Chunker test_chunk(file);
 
     std::vector<Chunk> a(test_chunk.ChunkFile());
 
-    File file1("/home/dantedoyl/watch/20newnew.txt");
+    File file1("/home/dantedoyl/watch/asd1.docx");
     Chunker test_chunk1(file1);
 
-    //test_chunk1.MergeFile(a);
+    test_chunk1.MergeFile(a);
     for(int i = 0; i < a.size(); i++){
       a[i].chunkId = i;
     }
